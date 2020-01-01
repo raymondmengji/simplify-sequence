9d1c337d864bad61d1bad912ed8861dd6767061e
Raymond Ji <raymondji@berkeley.edu>
2020-01-01 15:08:55 -0800

    added sub-sequence

--- myapp.py
+++ myapp.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+def remove_stuff(x):
+    answer = ""
+
+    for letter in x:
+        if letter.isalpha():
+            answer += letter
+    return answer
 
 @app.route("/")
 def home():
@@ -12,16 +19,23 @@ def hello():
     first_index = request.form['FirstIndex']
     second_index = request.form['SecondIndex']
 
-    def remove_stuff(x):
-        answer = ""
-
-        for letter in x:
-            if letter.isalpha():
-                answer += letter
-        return answer
-
     new_sequence = remove_stuff(sequence)
     return new_sequence[int(first_index)-1:int(second_index)]
+
+@app.route("/find", methods=["POST"])
+def find():
+    sequence = request.form['sequence']
+    sub_sequence = request.form['sub_sequence']
+    
+    new_sequence = remove_stuff(sequence)
+    new_sub_sequence = remove_stuff(sub_sequence)
+    
+    indexes = []
+    for i in range(len(new_sequence)):
+        if new_sequence[i:i+len(new_sub_sequence)] == new_sub_sequence:
+            indexes.append([i+1, i+len(new_sub_sequence)])
+    return str(indexes)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
