ffdf33b70961971e9bafc777ad7d10592f396836
Raymond Ji <raymondji@berkeley.edu>
2020-01-01 15:55:33 -0800

    added multiple sub-sequence functionality

--- myapp.py
+++ myapp.py
@@ -26,15 +26,36 @@ def hello():
 def find():
     sequence = request.form['sequence']
     sub_sequence = request.form['sub_sequence']
+    new_sequence = remove_stuff(sequence).lower()
+    sub_sequence_dict = {}
+
+    prev_index = 0
+    for i in range(len(sub_sequence)):
+        if sub_sequence[i] == ',':
+            sub_sequence_dict[remove_stuff(sub_sequence[prev_index:i]).lower()] = []
+            prev_index = i+1
+    #get the last sub_sequence
+    sub_sequence_dict[remove_stuff(sub_sequence[prev_index:len(sub_sequence)]).lower()] = []
+
+    def find_indexes(sub_sequence):
+        indexes = []
+        for i in range(len(new_sequence)):
+            if new_sequence[i:i+len(sub_sequence)] == sub_sequence:
+                indexes.append([i+1, i+len(sub_sequence)])
+        return indexes
+
+    for key in sub_sequence_dict:
+        sub_sequence_dict[key] = find_indexes(key)
+
+    #format string
+    return_string = ""
+    for key in sub_sequence_dict:
+        if len(sub_sequence_dict[key]) == 0:
+            return_string += key + ": No site<br />"
+        else:
+            return_string += key + " Number: " + str(len(sub_sequence_dict[key])) + " Indexes: " + str(sub_sequence_dict[key]) + "<br/>"
     
-    new_sequence = remove_stuff(sequence)
-    new_sub_sequence = remove_stuff(sub_sequence)
-    
-    indexes = []
-    for i in range(len(new_sequence)):
-        if new_sequence[i:i+len(new_sub_sequence)] == new_sub_sequence:
-            indexes.append([i+1, i+len(new_sub_sequence)])
-    return str(indexes)
+    return return_string.strip()
 
 
 if __name__ == '__main__':
