ae173b63d1a02f506d6045924c2f4c2d6d89a727
Raymond Ji <raymondji@berkeley.edu>
2020-01-08 20:02:52 -0800

    adding multiple html files

--- myapp.py
+++ myapp.py
@@ -13,6 +13,11 @@ def remove_stuff(x):
 def home():
     return render_template('index.html')
 
+@app.route("/return_to_main")
+def return_to_main():
+    return render_template('index.html')
+
+
 @app.route("/hello", methods=["POST"])
 def hello():
     sequence = request.form['sequence']
@@ -20,7 +25,7 @@ def hello():
     second_index = request.form['SecondIndex']
 
     new_sequence = remove_stuff(sequence)
-    return new_sequence[int(first_index)-1:int(second_index)]
+    return render_template("hello.html", text=new_sequence[int(first_index)-1:int(second_index)])
 
 @app.route("/find", methods=["POST"])
 def find():
@@ -47,15 +52,25 @@ def find():
     for key in sub_sequence_dict:
         sub_sequence_dict[key] = find_indexes(key)
 
+    #sort by number of appearances
+    sorted_list = []
+    for key in sub_sequence_dict:
+        sorted_list.append((key, sub_sequence_dict[key]))
+
+    sorted_list.sort(key=lambda x: len(x[1]))
+
+
     #format string
     return_string = ""
-    for key in sub_sequence_dict:
-        if len(sub_sequence_dict[key]) == 0:
-            return_string += key + ": No site<br />"
+    for val in sorted_list:
+        if len(val[1]) == 0:
+            return_string += val[0] + ": No site<br />"
         else:
-            return_string += key + " Number: " + str(len(sub_sequence_dict[key])) + " Indexes: " + str(sub_sequence_dict[key]) + "<br/>"
+            return_string += val[0] + " Number: " + str(len(val[1])) + " Indexes: " + str(val[1]) + "<br/>"
     
     return return_string.strip()
+
+
 
 
 if __name__ == '__main__':
